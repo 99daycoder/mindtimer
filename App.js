add356f06d2420735c553eaf5dce3d4bd3f24549
Phantom <sambaeverything@gmail.com>
2022-08-26 13:06:17 +0200

    Working app...

--- App.js
+++ App.js
@@ -1,53 +1,24 @@
-import { Button, Text, Input} from '@rneui/base';
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, View, Alert } from 'react-native';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './Components/Home';
+import Notes from './Components/Notes';
 
-export default function App() {
 
-  const simpleAlertHandler = () => {
-    //function to make simple alert
-    alert('Hello Its Time to log your progress');
-    //push to a new page here
-  };
-  const [alertsPerHour, setAlertsPerHour] = useState(0)
-  const [inputValue, setInputValue]= useState(0)
 
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Input
-      placeholder='How Many Alerts Per Hour?'onChangeText={newText => setInputValue(newText)}/>
-      <StatusBar style="auto" />
-      <Button onPress={()=>setAlertsPerHour(inputValue)}>Start Alerts</Button>
-      <Text>{alertsPerHour}</Text>
-      <Button onPress={simpleAlertHandler}>Show Alert</Button>
-      <Text>Hello</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home Page" component={Home} />
+        <Stack.Screen name="Notes" component={Notes} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    minWidth: '80%',
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-
-  h1Style: {
-    
-    fontWeight: '300',
-    color:'red'
-  },
-  h2Style: {
-    
-    fontWeight: '100',
-  },
-  h3Style: {
-    
-    fontWeight: '500',
-  },
-});
-
+export default App;
--- Components/Home.js
+++ Components/Home.js
@@ -1,31 +1,43 @@
-import { Button, Text, Input} from '@rneui/base';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Notes from './Notes';
-
+import { Button, Text, Input} from '@rneui/base';
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, View, Alert } from 'react-native';
 
 export default function Home({navigation}) {
+
+  const simpleAlertHandler = () => {
+    //function to make simple alert
+    alert('Hello Its Time to log your progress');
+    navigation.navigate('Notes')
+    //push to a new page here
+  };
+  const [alertsPerHour, setAlertsPerHour] = useState(0)
+  const [inputValue, setInputValue]= useState(0)
+
   return (
     <View style={styles.container}>
-      <View><Text style={styles.h1Style}>Random Bells per hour?</Text>
       <Input
-      placeholder='How Many Alerts / Hour?'/>
-      </View>
+      placeholder='How Many Alerts Per Hour?'onChangeText={newText => setInputValue(newText)}/>
       <StatusBar style="auto" />
-      <Button onPress={() => navigation.navigate('Notes')} >Start Timer</Button>
+      <Button onPress={()=>setAlertsPerHour(inputValue)}>Start Alerts</Button>
+      <Text>{alertsPerHour}</Text>
+      <Button onPress={simpleAlertHandler}>Show Alert</Button>
+      <Text>Hello</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    minWidth: '80%',
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 
   h1Style: {
     
