5d3414aed396f35f0dd70a8d4cae7857d253621d
Joe Klakus <joeyk2k@hotmail.com>
2022-08-26 11:22:23 +0100

    added button functionality

--- App.js
+++ App.js
@@ -1,29 +1,35 @@
 import { Button, Text, Input} from '@rneui/base';
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
-
-
 export default function App() {
+
+  const [alertsPerHour, setAlertsPerHour] = useState(0)
+  const [inputValue, setInputValue]= useState(0)
+
+  
+
   return (
     <View style={styles.container}>
-      <View><Text style={styles.h1Style}>Random Bells per hour?</Text>
       <Input
-      placeholder='How Many Alerts / Hour?'/>
-      </View>
+      placeholder='How Many Alerts Per Hour?'onChangeText={newText => setInputValue(newText)}/>
       <StatusBar style="auto" />
-      <Button>Start Timer</Button>
+      <Button onPress={()=>setAlertsPerHour(inputValue)}>Start Alerts</Button>
+      <Text>{alertsPerHour}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    minWidth: '80%',
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 
   h1Style: {
     
