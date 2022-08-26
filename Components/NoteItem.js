d898bfdfbbc5a5c8b241dfc0cebd177d4e162660
Phantom <sambaeverything@gmail.com>
2022-08-26 18:23:07 +0200

    items are now added to the list with today's date

--- Components/NoteItem.js
+++ Components/NoteItem.js
@@ -4,9 +4,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, ScrollView, Image} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-export default function NoteItem({note}) {
+export default function NoteItem({note, date}) {
   return (
     <View>
+        <Text>{date}</Text>
         <Text>{note}</Text>
     </View>
   )
--- Components/Notes.js
+++ Components/Notes.js
@@ -1,35 +1,45 @@
-import { Text, Input, Button} from '@rneui/base';
+import { Input, Button} from '@rneui/base';
 import { Card } from "@rneui/themed";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, ScrollView, Image } from 'react-native';
+import { StyleSheet, View, ScrollView, Image, Text, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import NoteItem from './NoteItem';
 import { useState } from 'react';
 
-
-
 export default function Notes({route, navigation}) {
 
   const [inputValue, setInputValue]= useState()
-  const [notes, setNotes] = useState(['Eating a banana', 'WC', 'Netflix', 'earting crisp', 'playing PS4'])
+  const [notes, setNotes] = useState([{item:'Eating a banana', date:''}, {item:'Eating chips', date:''},{item:'Creating presentation', date:''}])
 
   const simpleNoteHandler = () => {
-    setNotes([...notes, inputValue])
+    setNotes([...notes, {item:inputValue}])
+    console.log('this is simple note handler...')
     
   };
   const [alertsPerHour, setAlertsPerHour] = useState(0)
+  //Today's date
+  var date = new Date().getDate();
+      var month = new Date().getMonth() + 1;
+      var year = new Date().getFullYear();
+ 
+      //Alert.alert(date + '-' + month + '-' + year);
+      // You can turn it in to your desired format
+      const todaysDate =  date + '-' + month + '-' + year;//format: d-m-y;
+
+  console.log(notes[0].item)
   return (
     <View style={styles.container}>
       <Text style={styles.h1Style}> Your Alert is set to {route.params.alertsPerHour} </Text>
-     <View style={styles.addNotes}><Input placeholder='What were you doing now?' onChangeText={newText => setInputValue(newText)}></Input> 
-      <Button title="Add Item" onPress={simpleNoteHandler}/></View> 
-      <Card.Title>What you've been up to</Card.Title>
+     <View style={styles.addNotes}><TextInput style={styles.addNotesText} placeholder='What were you doing now?' onChangeText={newText => setInputValue(newText)}></TextInput> 
+      <Button style={styles.addNotesButton} title="Add Item" onPress={simpleNoteHandler}/></View> 
+      <Card.Title>What you've been up to today</Card.Title>
         <Card.Divider/>
         <View style={{position:"relative",alignItems:"center"}}>
          
           {notes.map((item, i)=>
-     <NoteItem key={i} note={item}/>
+          
+     <NoteItem key={i} note={item.item} date={todaysDate} />
       
      )}
          </View>
@@ -41,8 +51,7 @@ const styles = StyleSheet.create({
   container: {
     flex:1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
   },
 
   h1Style: {
@@ -61,9 +70,19 @@ const styles = StyleSheet.create({
   addNotes: {
   display: "flex",
   flexDirection: 'row',
-  marginLeft: 20,
-  maxWidth: '80%'
+  alignContent: 'center',
+  alignItems: 'center',
+  alignSelf:'center',
+paddingRight:20,
+paddingLeft:30,
 
-  }
+  },
+  addNotesText: {
+   
+    },
+  addNotesButton: {
+      
+      },
+
 });
 
