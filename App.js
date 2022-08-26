0dcf0cad5a21b0d2c8900eb18e1defa724628102
Phantom <sambaeverything@gmail.com>
2022-08-26 11:30:02 +0200

    skeleton of phone app

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import { Button, Text} from '@rneui/base';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.h1Style}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button>This is button</Button>
     </View>
   );
 }
@@ -17,4 +20,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  h1Style: {
+    
+    fontWeight: '300',
+    color:'red'
+  },
+  h2Style: {
+    
+    fontWeight: '100',
+  },
+  h3Style: {
+    
+    fontWeight: '500',
+  },
 });
+
---
+++ readme.md
@@ -0,0 +1,4 @@
+Steps taken:
+- npx create-expo-app .
+- npm install @rneui/themed @rneui/base
+- expo install expo-font
