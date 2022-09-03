87ff8f4972595acf5340dc5920980be187f6d396
Phantom <sambaeverything@gmail.com>
2022-09-03 17:19:54 +0200

    added local storage

--- Components/NoteItem.js
+++ Components/NoteItem.js
@@ -1,13 +1,16 @@
 import { Text } from '@rneui/base';
 import { Card } from "@rneui/themed";
 import { StyleSheet} from 'react-native';
+import { TouchableOpacity } from 'react-native-web';
 import { globalStyles } from '../styles/global';
 
-export default function NoteItem({note, date}) {
+export default function NoteItem({note, date, deleteHandler, item}) {
   return (
-    <Card>
+   <TouchableOpacity onPress={deleteHandler(item.key)}>
+   <Card>
         <Text style={globalStyles.h2Style}>{date}</Text>
         <Text style={globalStyles.h1Style} >{note}</Text>
     </Card>
+    </TouchableOpacity>
   )
 }
--- Components/Notes.js
+++ Components/Notes.js
@@ -2,21 +2,51 @@ import { Input, Button } from '@rneui/base';
 import { Card } from "@rneui/themed";
 import { View, Keyboard, FlatList } from 'react-native';
 import NoteItem from './NoteItem';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { globalStyles } from '../styles/global';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Notes() {
-
   let input = useRef()
   const [inputValue, setInputValue] = useState()
-  const [notes, setNotes] = useState([{ item: 'Eating a banana', date: '' }, { item: 'Eating chips', date: '' }, { item: 'Creating presentation', date: '' }])
+  const [notes, setNotes] = useState()
 
+  useEffect(() => {
+   getData()
+  }, [])
+
+  useEffect(() => {
+    storeData(notes)  
+  }, [notes])
+  
+  // Store notes to local storage
+  const storeData = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value)
+      await AsyncStorage.setItem('@storage_Key', jsonValue)
+    } catch (e) {
+      // saving error
+    }
+  }
+// Get value of notes
+const getData = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem('@storage_Key')
+    console.log('*jsonValue is..', jsonValue)
+     jsonValue != null ? setNotes(JSON.parse(jsonValue)) : null;
+  } catch(e) {
+    // error reading value
+  }
+}
   const simpleNoteHandler = () => {
-    setNotes([...notes, { item: inputValue }])
+    console.log('this is notes', notes)
+    setNotes([...notes, { item: inputValue , key: Math.random().toString(4)}])
+    // storeData([...notes, { item: inputValue }])
     input.current.clear();
     Keyboard.dismiss();
-  };
-
+  }
+  
+  
   //Today's date
   var date = new Date().getDate();
   var month = new Date().getMonth() + 1;
@@ -25,6 +55,10 @@ export default function Notes() {
   //Alert.alert(date + '-' + month + '-' + year);
   // You can turn it in to your desired format
   const todaysDate = date + '-' + month + '-' + year;//format: d-m-y;
+
+  function deleteHandler(){
+    setNotes(notes.filter(note => note.key === key))
+  }
 
   return (
     <View style={globalStyles.container}>
@@ -41,7 +75,7 @@ export default function Notes() {
         <FlatList
               data={notes}
               renderItem={({ item }) => (
-                <NoteItem key={todaysDate} note={item.item} date={todaysDate} />
+                <NoteItem deleteHandler={deleteHandler}  note={item.item} date={todaysDate} item={item}/>
               )}
             />
     </View>
