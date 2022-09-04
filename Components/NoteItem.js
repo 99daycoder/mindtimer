a169f976532f8c1c14d3c7dafdb91f9124d148eb
Joe Klakus <joeyk2k@hotmail.com>
2022-09-04 20:42:18 +0100

    rubbish icon

--- Components/NoteItem.js
+++ Components/NoteItem.js
@@ -1,16 +1,18 @@
 import { Text } from '@rneui/base';
 import { Card } from "@rneui/themed";
-import { StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native';
 import { globalStyles } from '../styles/global';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function NoteItem({note, deleteHandler}) {
+export default function NoteItem({ note, deleteHandler }) {
   return (
-   <TouchableOpacity onPress={() => deleteHandler(note.key)}>
-   <Card>
-        <Text style={globalStyles.h2Style}>{note.date}</Text>
-        <Text style={globalStyles.h1Style}>{note.item}</Text>
+    <Card>
+      <TouchableOpacity onPress={() => deleteHandler(note.key)}>
+        <Ionicons name={'trash'} size={30}/>
+      </TouchableOpacity>
+      <Text style={globalStyles.h2Style}>{note.date}</Text>
+      <Text style={globalStyles.h1Style}>{note.item}</Text>
     </Card>
-    </TouchableOpacity>
   )
 }
