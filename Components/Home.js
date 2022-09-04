6b3c92d3089586de94c38a45781540da5fc74d50
Phantom <sambaeverything@gmail.com>
2022-09-04 22:30:08 +0200

    hide keyboard

--- Components/Home.js
+++ Components/Home.js
@@ -1,20 +1,20 @@
-import { Button, Input, Card } from '@rneui/base';
-import { StatusBar } from 'expo-status-bar';
-import { useState, useRef, useEffect } from 'react';
-import { View, Alert, Keyboard } from 'react-native';
-import { globalStyles } from '../styles/global';
+import { Button, Input, Card } from "@rneui/base";
+import { StatusBar } from "expo-status-bar";
+import { useState, useRef, useEffect } from "react";
+import { View, Alert, Keyboard, TouchableWithoutFeedback } from "react-native";
+import { globalStyles } from "../styles/global";
 import * as Notifications from "expo-notifications";
-import * as Device from 'expo-device';
-
-
+import { TouchableOpacity } from 'react-native';
+import * as Device from "expo-device";
 
 
 export default function Home({ navigation }) {
-
   useEffect(() => {
-    const subscription = Notifications.addNotificationResponseReceivedListener(notification => {
-      navigation.navigate('Notes')
-    });
+    const subscription = Notifications.addNotificationResponseReceivedListener(
+      (notification) => {
+        navigation.navigate("Notes");
+      }
+    );
     return () => subscription.remove();
   }, []);
 
@@ -26,20 +26,19 @@ export default function Home({ navigation }) {
     }),
   });
 
-  let input = useRef()
-  const [alertsPerHour, setAlertsPerHour] = useState(0)
-  const [inputValue, setInputValue] = useState(0)
+  let input = useRef();
+  const [alertsPerHour, setAlertsPerHour] = useState(0);
+  const [inputValue, setInputValue] = useState(0);
 
   async function scheduleAndCancel() {
     await Notifications.scheduleNotificationAsync({
       content: {
-        title: 'MindTimer Notification',
+        title: "MindTimer Notification",
         body: "Please log your activity",
-        data: { data: 'goes here' }
+        data: { data: "goes here" },
       },
       trigger: { seconds: parseInt(inputValue), repeats: true },
     });
-
   }
 
   const startAlertHandler = () => {
@@ -47,65 +46,70 @@ export default function Home({ navigation }) {
     Keyboard.dismiss();
 
     // Alert.alert(`Please Log your activity!!`);
-    scheduleAndCancel()
+    scheduleAndCancel();
     // navigation.navigate('Notes')
-
-  }
+  };
   const stopAlertHandler = async () => {
     await Notifications.cancelAllScheduledNotificationsAsync();
-    setAlertsPerHour(0)
-  }
+    setAlertsPerHour(0);
+  };
   const registerForPushNotificationsAsync = async () => {
     if (Device.isDevice) {
-      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
       let finalStatus = existingStatus;
-      if (existingStatus !== 'granted') {
+      if (existingStatus !== "granted") {
         const { status } = await Notifications.requestPermissionsAsync();
         finalStatus = status;
       }
-      if (finalStatus !== 'granted') {
-        alert('Failed to get push token for push notification!');
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
         return;
       }
       const token = (await Notifications.getExpoPushTokenAsync()).data;
       console.log(token);
       this.setState({ expoPushToken: token });
     } else {
-      alert('Must use physical device for Push Notifications');
+      alert("Must use physical device for Push Notifications");
     }
-  
-    if (Platform.OS === 'android') {
-      Notifications.setNotificationChannelAsync('default', {
-        name: 'default',
+
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
         importance: Notifications.AndroidImportance.MAX,
         vibrationPattern: [0, 250, 250, 250],
-        lightColor: '#FF231F7C',
+        lightColor: "#FF231F7C",
       });
     }
-    };
+  };
   return (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
     <View style={globalStyles.container}>
-      <Input
-        ref={input}
-        placeholder='How Many Seconds Between Alerts?'
-        onEndEditing={() => setAlertsPerHour(inputValue)}
-        onChangeText={newText => setInputValue(newText)} />
-      <StatusBar style="auto" />
-      {(alertsPerHour > 0) ?
-        <Card.Title>You are currently seeing alerts every {alertsPerHour} seconds</Card.Title>
-        :
-        <Card.Title>Your alerts are currently off</Card.Title>}
-      <Button
-        title="Start Alerts"
-        onPress={startAlertHandler}
-        buttonStyle={globalStyles.button} />
-      <Button
-        title="Stop Alerts"
-        onPress={stopAlertHandler}
-        buttonStyle={globalStyles.button} />
-    </View>
+        <Input
+          ref={input}
+          placeholder="How Many Seconds Between Alerts?"
+          onEndEditing={() => setAlertsPerHour(inputValue)}
+          onChangeText={(newText) => setInputValue(newText)}
+        />
+        <StatusBar style="auto" />
+        {alertsPerHour > 0 ? (
+          <Card.Title>
+            You are currently seeing alerts every {alertsPerHour} seconds
+          </Card.Title>
+        ) : (
+          <Card.Title>Your alerts are currently off</Card.Title>
+        )}
+        <Button
+          title="Start Alerts"
+          onPress={startAlertHandler}
+          buttonStyle={globalStyles.button}
+        />
+        <Button
+          title="Stop Alerts"
+          onPress={stopAlertHandler}
+          buttonStyle={globalStyles.button}
+        />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
-
-
-
--- Components/NoteItem.js
+++ Components/NoteItem.js
@@ -4,15 +4,21 @@ import { StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native';
 import { globalStyles } from '../styles/global';
 import { Ionicons } from '@expo/vector-icons';
+import { View } from 'react-native';
 
 export default function NoteItem({ note, deleteHandler }) {
   return (
-    <Card>
+
+    <Card style={globalStyles.row}>
+      <View style={globalStyles.cardRow}>
+      <Text style={globalStyles.h2Style}>{note.date}</Text>
+      <Text style={globalStyles.h1Style}>{note.item}</Text>
       <TouchableOpacity onPress={() => deleteHandler(note.key)}>
         <Ionicons name={'trash'} size={30}/>
       </TouchableOpacity>
-      <Text style={globalStyles.h2Style}>{note.date}</Text>
-      <Text style={globalStyles.h1Style}>{note.item}</Text>
+      
+      
+      </View>
     </Card>
   )
 }
