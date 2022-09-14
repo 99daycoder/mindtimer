ad5d77d870ab42c404298ac767d967ebcb320cd1
Joe Klakus <joeyk2k@hotmail.com>
2022-09-14 10:35:57 +0100

    fixed slider

--- Components/Home.js
+++ Components/Home.js
@@ -1,11 +1,9 @@
 import { Button, Input, Card } from "@rneui/base";
 import { StatusBar } from "expo-status-bar";
 import { useState, useRef, useEffect } from "react";
-import { View, Alert, Keyboard, TouchableWithoutFeedback } from "react-native";
+import { View, Keyboard, TouchableWithoutFeedback } from "react-native";
 import { globalStyles } from "../styles/global";
 import * as Notifications from "expo-notifications";
-import { TouchableOpacity } from 'react-native';
-import * as Device from "expo-device";
 import Slider from '@react-native-community/slider';
 
 
@@ -27,12 +25,11 @@ export default function Home({ navigation }) {
       shouldSetBadge: false,
     }),
   });
-
   let input = useRef();
   const [alertsPerHour, setAlertsPerHour] = useState(0);
   const [inputValue, setInputValue] = useState(0);
 
-  async function scheduleAndCancel() {
+  const startAlertHandler = async () => {
     await Notifications.scheduleNotificationAsync({
       content: {
         title: "MindTimer Notification",
@@ -41,49 +38,12 @@ export default function Home({ navigation }) {
       },
       trigger: { seconds: parseInt(inputValue), repeats: true },
     });
-  }
-
-  const startAlertHandler = () => {
-    input.current.clear();
-    Keyboard.dismiss();
-
-    // Alert.alert(`Please Log your activity!!`);
-    scheduleAndCancel();
-    // navigation.navigate('Notes')
   };
   const stopAlertHandler = async () => {
     await Notifications.cancelAllScheduledNotificationsAsync();
     setAlertsPerHour(0);
   };
-  const registerForPushNotificationsAsync = async () => {
-    if (Device.isDevice) {
-      const { status: existingStatus } =
-        await Notifications.getPermissionsAsync();
-      let finalStatus = existingStatus;
-      if (existingStatus !== "granted") {
-        const { status } = await Notifications.requestPermissionsAsync();
-        finalStatus = status;
-      }
-      if (finalStatus !== "granted") {
-        alert("Failed to get push token for push notification!");
-        return;
-      }
-      const token = (await Notifications.getExpoPushTokenAsync()).data;
-      console.log(token);
-      this.setState({ expoPushToken: token });
-    } else {
-      alert("Must use physical device for Push Notifications");
-    }
 
-    if (Platform.OS === "android") {
-      Notifications.setNotificationChannelAsync("default", {
-        name: "default",
-        importance: Notifications.AndroidImportance.MAX,
-        vibrationPattern: [0, 250, 250, 250],
-        lightColor: "#FF231F7C",
-      });
-    }
-  };
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={globalStyles.container}>
